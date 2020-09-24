ea8986cb05cf261cb08f0602fa82de613611ea45
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-09-23 20:24:30 -0700

    upgrade: Glide, Kotlin, and other libraries to the latest versions

--- app/src/main/java/com/maruiz/glideexample/presentation/adapter/BooksAdapter.kt
+++ app/src/main/java/com/maruiz/glideexample/presentation/adapter/BooksAdapter.kt
@@ -7,18 +7,18 @@ import com.maruiz.glideexample.R
 import com.maruiz.glideexample.presentation.extensions.inflate
 import com.maruiz.glideexample.presentation.extensions.loadImage
 import com.maruiz.glideexample.presentation.presentationmodel.BookPresentationModel
-import kotlinx.android.synthetic.main.row_book.view.*
+import kotlinx.android.synthetic.main.row_book.view.image
 import kotlin.properties.Delegates
 import kotlin.time.ExperimentalTime
-import kotlin.time.MonoClock
+import kotlin.time.TimeSource
 
 class BooksAdapter : RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
     var renderables: List<BookPresentationModel> by Delegates.observable(emptyList()) { _, _, _ ->
         notifyDataSetChanged()
     }
 
-    @UseExperimental(ExperimentalTime::class)
-    private val totalMark by lazy { MonoClock.markNow() }
+    @OptIn(ExperimentalTime::class)
+    private val totalMark by lazy { TimeSource.Monotonic.markNow() }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(parent.inflate(R.layout.row_book))
